dc072a2eead413ae487ea990cdf77ccf332be649
Bruno Quintella <bruno.quintella@bravi.com.br>
2017-03-28 21:39:02 +0100

    Use guard to validate the url

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent }  from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-guard-service';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
@@ -18,7 +19,11 @@ import { StarComponent } from './shared/star.component';
     HttpModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: 'product/:id', component: ProductDetailComponent },
+      { 
+        path: 'product/:id', 
+        canActivate: [ ProductDetailGuard ],
+        component: ProductDetailComponent 
+      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
@@ -32,6 +37,7 @@ import { StarComponent } from './shared/star.component';
     ProductFilterPipe, 
     StarComponent 
   ],
+  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
