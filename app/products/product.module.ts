a86df34c7336c509b78f528cf165d8375672a645
Bruno Quintella <bruno.quintella@bravi.com.br>
2017-04-01 18:11:34 +0100

    Refactor the product module into product module and shared module

--- app/products/product.module.ts
+++ app/products/product.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list.component';
@@ -8,18 +6,17 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductDetailGuard } from './product-guard-service';
 import { ProductService } from './product.service';
-import { StarComponent } from '../shared/star.component';
+
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
-        ProductFilterPipe,
-        StarComponent
+        ProductFilterPipe
     ],
     imports: [
-        FormsModule,
-        CommonModule,
+        SharedModule,
         RouterModule.forChild([
             { path: 'products', component: ProductListComponent },
             { path: 'product/:id', canActivate: [ ProductDetailGuard ], component: ProductDetailComponent }
